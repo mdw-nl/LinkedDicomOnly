b2a1c11c8dcaff0b55e40bfa4d01ce96fb2b41e4
alessioromita <romitaalessio@gmail.com>
2023-10-26 09:34:55 +0200

    Upload to graph db function moved to utils.py

--- LinkedDicom/cli.py
+++ LinkedDicom/cli.py
@@ -4,41 +4,10 @@ from LinkedDicom import LinkedDicom
 from LinkedDicom.rt import dvh
 import os
 import click
-import requests
-from .util import check_if_exist
+
+from .util import upload_graph_db
 from uuid import uuid4
-def upload_graph_db(graphdb_url, repository_id, data_file, contenttype):
-    """
-    Import the data in graphdb based on the tupe specified by contetype(json/ttl tested)
 
-    @param graphdb_url:
-    @param repository_id:
-    @param json_data_path:
-    @return:
-    """
-
-    # Define the URL for the GraphDB REST API endpoint to upload data
-    upload_url = f"{graphdb_url}/repositories/{repository_id}/statements"
-
-    # Load JSON-LD data from a file
-
-    with open(data_file, "r") as file:
-        data = file.read()
-
-    # Set headers for the HTTP request
-    headers = {
-        "Content-Type": contenttype,
-    }
-
-    # Send a POST request to upload the data to GraphDB
-    response = requests.post(upload_url, data=data, headers=headers)
-
-    # Check the response
-    if response.status_code in (200, 204):
-        print("Data imported successfully.")
-    else:
-        print("Error importing data. Status code:", response.status_code)
-        print("Response content:", response.content)
 
 
 @click.command()
--- LinkedDicom/util.py
+++ LinkedDicom/util.py
@@ -1,5 +1,7 @@
 import os
 
+import requests
+
 
 def save_list(my_list, path):
     with open(path, "w") as file:
@@ -22,5 +24,39 @@ def read_list(path):
 def check_if_exist(path):
     return os.path.exists(path)
 
+
+def upload_graph_db(graphdb_url, repository_id, data_file, contenttype):
+    """
+    Import the data in graphdb based on the tupe specified by contetype(json/ttl tested)
+
+    @param graphdb_url:
+    @param repository_id:
+    @param contenttype:
+    @param data_file:
+    @return:
+    """
+
+    # Define the URL for the GraphDB REST API endpoint to upload data
+    upload_url = f"{graphdb_url}/repositories/{repository_id}/statements"
+
+    # Load JSON-LD data from a file
+
+    with open(data_file, "r") as file:
+        data = file.read()
+
+    # Set headers for the HTTP request
+    headers = {
+        "Content-Type": contenttype,
+    }
+
+    # Send a POST request to upload the data to GraphDB
+    response = requests.post(upload_url, data=data, headers=headers)
+
+    # Check the response
+    if response.status_code in (200, 204):
+        print("Data imported successfully.")
+    else:
+        print("Error importing data. Status code:", response.status_code)
+        print("Response content:", response.content)
 
 # Reading the list back from the plain text file
