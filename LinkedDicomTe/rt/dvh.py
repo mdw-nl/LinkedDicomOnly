d1214cefa2fdc786ac2bccb0fe9c09a52d5faa40
alessioromita <romitaalessio@gmail.com>
2023-12-12 20:07:02 +0100

    fix issue if mask fail

--- LinkedDicomTe/rt/dvh.py
+++ LinkedDicomTe/rt/dvh.py
@@ -66,6 +66,8 @@ def get_dvh_v(structure,
         different formats using the attributes and properties of the DVH class.
     """
 
+
+    #rtplan = rtplan.replace("/data/pre-act/mnt/", "/Volumes/research/Projects/cds/p0630-pre-act-dm/")
     rtss = dicomparser.DicomParser(structure)
     rtdose = dicomparser.DicomParser(dose, memmap_pixel_array=memmap_rtdose)
     structures = rtss.GetStructures()
@@ -160,6 +162,8 @@ SELECT  distinct ?patientID ?rtDose ?rtStruct ?rtDosePath ?rtStructPath ?rtPlanP
 					?fg ldcm:has_sequence_item ?fgg.
 					?fgg ldcm:T300A0078 ?fgn.
 					
+                   
+					
     
     
                 }
@@ -193,8 +197,8 @@ SELECT  distinct ?patientID ?rtDose ?rtStruct ?rtDosePath ?rtStructPath ?rtPlanP
             try:
                 calculatedDose = self.__get_dvh_for_structures(dosePackage.rtStructPath, dosePackage.rtDosePath,
                                                                dosePackage.rtPlanPath)
-            except:
-                continue
+            except Exception as  ex:
+               continue
             logging.info("Calculation Complete ")
             uuid_for_calculation = uuid4()
             resultDict = {
@@ -323,17 +327,24 @@ SELECT  distinct ?patientID ?rtDose ?rtStruct ?rtDosePath ?rtStructPath ?rtPlanP
 
         if type(rtStructPath) == rdflib.term.URIRef:
             rtStructPath = str(rtStructPath).replace("file://", "")
+        #rtStructPath = rtStructPath.replace("/data/pre-act/mnt/", "/Volumes/research/Projects/cds/p0630-pre-act-dm/")
         structObj = dicomparser.DicomParser(rtStructPath)
 
         if type(rtDosePath) == rdflib.term.URIRef:
             rtDosePath = str(rtDosePath).replace("file://", "")
+        #rtDosePath = rtDosePath.replace("/data/pre-act/mnt/", "/Volumes/research/Projects/cds/p0630-pre-act-dm/")
+
 
         structures = structObj.GetStructures()
         dvh_list = []
         for index in structures:
             logging.info("Calculating structures " + str(structures[index]))
             structure = structures[index]
-            calcdvh = get_dvh_v(rtStructPath, rtDosePath, index, rtPlan)
+            try:
+                calcdvh = get_dvh_v(rtStructPath, rtDosePath, index, rtPlan)
+            except:
+                logging.warning("Skipping...")
+                continue
             dvh_d = calcdvh.bincenters.tolist()
 
             dvh_v = calcdvh.counts.tolist()
