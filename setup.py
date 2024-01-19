bd33b9bb7678301a79c25fcd8f72c4f014912b89
alessioromita <romitaalessio@gmail.com>
2024-01-19 17:40:44 +0100

    fix issue

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     description='A package to extract DICOM header data and store this in RDF',
     long_description="A package to extract DICOM header data and store this in RDF",
     install_requires=[
-        "pandas",
         "pydicom",
+        "pandas",
         "rdflib",
         "requests",
         "click",
@@ -25,8 +25,8 @@ setup(
             'ldcm-parse-test = LinkedDicomTe.cli:main_parse_test',
             'ldcm-calc-dvh = LinkedDicomTe.cli:calc_dvh',
             'ldcm-scp = LinkedDicomTe.CLI_SCP:start_scp',
-            'ldcm-upload = LinkedDicomTe.cli:upload_graph'
-            'ldcm-dvh-from-file=LinkedDicomTe.cli:DVH_from_folder_file'
+            'ldcm-upload = LinkedDicomTe.cli:upload_graph',
+            'ldcm-dvh-from-file = LinkedDicomTe.cli:DVH_from_folder_file'
         ]
     },
     package_data = {
